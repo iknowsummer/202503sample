6ca2edd72ad2db81ced5d52c7bdf1e9f866612e5
iknowsummer <iknowsummer@gmail.com>
2025-03-08 15:12:54 +0900

    [django] reactの読み込み先をビルド先直接に変更

--- app/app/settings.py
+++ app/app/settings.py
@@ -62,9 +62,7 @@ ROOT_URLCONF = "app.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [
-            os.path.join(BASE_DIR, "app", "static", "build"),
-        ],
+        "DIRS": [os.path.join(BASE_DIR, "../frontend/build/")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -127,9 +125,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "app", "static", "build", "static"),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "../frontend/build/static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
