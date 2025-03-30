e5e4e6b37aa290c3ac307eb4c135c47ad641ef3b
iknowsummer <iknowsummer@gmail.com>
2025-03-30 23:07:10 +0900

    django staticの設定をコメントでメモ

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,6 +19,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
+
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+
 from . import views
 from .views import CustomerViewSet
 
@@ -34,4 +39,6 @@ urlpatterns = [
     path("replace/", views.replace_customers, name="replace_customers"),
     path('export_customers/', views.export_customers, name='export_customers'),
     path('api/', include(router.urls)),
-]  # fmt: skip
+] # fmt: skip
+
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
