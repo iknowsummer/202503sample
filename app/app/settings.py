6f9a92a9801e21767e91910dadbf0297ae920708
iknowsummer <iknowsummer@gmail.com>
2025-03-30 22:53:25 +0900

    WhiteNoise追加（static配信のため）

--- app/app/settings.py
+++ app/app/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -133,11 +134,13 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "app/react_build/")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "app/react_build/static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # STATICFILES_DIRS = [BASE_DIR / "app/static"]
